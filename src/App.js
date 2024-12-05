37f4b56e8f645d07ef9ff2ad63c8b9f31fb8fbcb
Emorris70 <emorris7060@gmail.com>
2024-12-04 21:18:28 -0600

    modified App.js to EmileMorris Project 4 react app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Emorris Project 4 react App</h1>
+        <h1>Emilemorris Project 4 react App</h1>
       </header>
     </div>
   );
